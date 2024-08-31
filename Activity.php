464676d8110b914af06152e477230f0b2e4408d0
Reister Bricks Enriquez <147915838+enriquez141@users.noreply.github.com>
2024-08-31 16:27:55 +0800

    Add files via upload

---
+++ Activity.php
@@ -0,0 +1,212 @@
+<?php
+echo "Hands-on Activity";
+    $table = array(
+        "header"=>array(
+            "Student ID",
+            "Lastname",
+            "Middlename",
+            "Firstname",
+            "Cource",
+            "Section"
+        ),
+        "body"=>
+            array(
+            "lastname"=>"Enriquez",
+            "middlename"=>"Villanueva",
+            "firstname"=>"Reister Bricks",
+            "course"=>"BSIT",
+            "section"=>"3A"
+        ),
+        array(
+            "lastname"=>"Enriquez",
+            "middlename"=>"Villanueva",
+            "firstname"=>"Reister Bricks",
+            "course"=>"BSIT",
+            "section"=>"3A"
+        ),
+        array(
+            "lastname"=>"Enriquez",
+            "middlename"=>"Villanueva",
+            "firstname"=>"Reister Bricks",
+            "course"=>"BSIT",
+            "section"=>"3A"
+        ),
+        array(
+            "lastname"=>"Enriquez",
+            "middlename"=>"Villanueva",
+            "firstname"=>"Reister Bricks",
+            "course"=>"BSIT",
+            "section"=>"3A"
+        ),
+        array(
+            "lastname"=>"Enriquez",
+            "middlename"=>"Villanueva",
+            "firstname"=>"Reister Bricks",
+            "course"=>"BSIT",
+            "section"=>"3A"
+        ),
+        array(
+            "lastname"=>"Enriquez",
+            "middlename"=>"Villanueva",
+            "firstname"=>"Reister Bricks",
+            "course"=>"BSIT",
+            "section"=>"3A"
+        ),
+        array(
+            "lastname"=>"Enriquez",
+            "middlename"=>"Villanueva",
+            "firstname"=>"Reister Bricks",
+            "course"=>"BSIT",
+            "section"=>"3A"
+        ),
+        array(
+            "lastname"=>"Enriquez",
+            "middlename"=>"Villanueva",
+            "firstname"=>"Reister Bricks",
+            "course"=>"BSIT",
+            "section"=>"3A"
+        ),
+        array(
+            "lastname"=>"Enriquez",
+            "middlename"=>"Villanueva",
+            "firstname"=>"Reister Bricks",
+            "course"=>"BSIT",
+            "section"=>"3A"
+        ),
+        array(
+            "lastname"=>"Enriquez",
+            "middlename"=>"Villanueva",
+            "firstname"=>"Reister Bricks",
+            "course"=>"BSIT",
+            "section"=>"3A"
+        ),
+    );
+
+?>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>Student ID</th>
+            <th>Lastname</th>
+            <th>Midlename</th>
+            <th>Firstname</th>
+            <th>Cource</th>
+            <th>Section</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>0</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>1</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>2</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>3</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>4</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>5</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>6</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>7</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>8</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>9</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+    <thead>
+        <tr>
+            <th>10</th>
+            <th>Enriquez</th>
+            <th>Villanueva</th>
+            <th>Reister Bricks</th>
+            <th>BSIT</th>
+            <th>3C</th>
+        </tr>
+    </thead>
+
+
+
+
+
